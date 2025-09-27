9ecf93017f7ef184bef37abf88cad51f5e160f8e
huziqi <ziqihu@outlook.com>
2025-09-28 00:09:55 +0800

    init commit

---
+++ start-dev-server.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting Jekyll development server...
+echo.
+echo The website will be available at: http://localhost:4000
+echo Press Ctrl+C to stop the server
+echo.
+bundle exec jekyll serve --livereload --host localhost --port 4000 --config _config.yml,_config_dev.yml
